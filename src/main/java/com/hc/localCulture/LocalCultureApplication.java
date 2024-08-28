ab80a9f22e1011e29ee7482484079830757e6986
1231harsh <1206harsh@gmail.com>
2024-08-28 12:59:54 +0530

    added all the files (initial commit)

---
+++ src/main/java/com/hc/localCulture/LocalCultureApplication.java
@@ -0,0 +1,13 @@
+package com.hc.localCulture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalCultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hc/localCulture/LocalCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.localCulture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalCultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
